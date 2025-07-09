73c5403ba30ad825f5d43f5b9d618ed4ed79520b
Ankur Verma <ankur.verma@dxc.com>
2025-07-09 09:38:09 +0530

    'updates'

---
+++ assets/JobLists-DTPKZB7e.js
@@ -0,0 +1,8 @@
+import{j as s,J as t}from"./index-D93r4nfy.js";const c=({jobs:r,showSearch:i,searchTerm:a})=>s.jsx("div",{children:s.jsx("div",{className:`grid 
+        grid-cols-1 
+        sm:grid-cols-2 
+        md:grid-cols-3 
+        lg:grid-cols-4
+        
+         
+        gap-6 p-4 transition-all duration-300 ${i?"pt-40":""} max-w-screen-2xl mx-auto`,children:r.map((o,d)=>s.jsx(t,{job:o,searchTerm:a},d))})});export{c as default};
